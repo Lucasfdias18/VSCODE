d26323c2d33e126b0846a691a324b1c9520f5f48
lucas.ferreira <lucasfdias120@gmail.com>
2026-07-08 14:09:04 -0300

    Mdod

--- Adicionar.py
+++ Adicionar.py
@@ -74,15 +74,16 @@ opcao = st.radio(
 if opcao == "Novo Produto":
 
     produto = st.text_input("Nome do Produto")
-    id = st.number_input(
-        "ID do Produto",
-        min_value=1,
-        step=1
-    )
+    
     quantidade = st.number_input(
         "Quantidade Inicial",
         min_value=1,
         step=1
+    )
+    id = st.number_input(
+        "ID do Produto",
+        min_value=1,
+        step=1    
     )
 
     if st.button("Cadastrar Produto"):
